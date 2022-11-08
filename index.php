a6edb495dfe6be1d9be154015d48ecb0fbde8044
amine <amine.mohammed-el-amine.djellal@epitech.eu>
2022-11-08 15:03:29 +0100

    [ADD] algo

--- index.php
+++ index.php
@@ -3,30 +3,31 @@
 /**
  * Gestion des opérations .
  */
-function addition($a, $b)
+function addition($a, $b, &$result)
 {
-    return $a + $b;
+    $result += $a + $b;
 }
 
-function soustration($a, $b)
+function soustration($a, $b, &$result)
 {
-    return $a - $b;
+    $result += $a - $b;
 }
 
-function division($a, $b)
+function division($a, $b, &$result)
 {
-    return $a / $b;
+    $result += $a / $b;
 }
 
-function multiplication($a, $b)
+function multiplication($a, $b, &$result)
 {
-    return $a * $b;
+    $result += $a * $b;
 }
 
-function modulo($a, $b)
+function modulo($a, $b, &$result)
 {
-    return $a % $b;
+    $result += $a % $b;
 }
+
 
 
 /**
@@ -34,10 +35,31 @@ function modulo($a, $b)
  */
 function eval_expr(string $expr)
 {
+    $result = 0;
     //contenue de la recherche regex
     $matches = [];
 
     //regex servant a récuperer toutes les valeurs numerique y compris les decimaux compris entre les symbole de calcule
     preg_match_all("/(?<!\d)[-]?\d*\.?\d+|[\\%\\+\\-\\/\\*\\(\\)]/", $expr, $matches);
-    print_r($matches);
+    //reinitialisation du tableau matches .
+    $matches = $matches[0];
+    //print_r($matches);
+
+    foreach ($matches as $index => $res) {
+        if (!is_numeric($res)) {
+            //echo $index . " " . $res . "\n";
+            if ($res == "+") {
+                addition($matches[$index - 1], $matches[$index + 1], $result);
+            } elseif ($res == "-") {
+                soustration($matches[$index - 1], $matches[$index + 1], $result);
+            } elseif ($res == "/") {
+                division($matches[$index - 1], $matches[$index + 1], $result);
+            } elseif ($res == "*") {
+                multiplication($matches[$index - 1], $matches[$index + 1], $result);
+            } elseif ($res == "%") {
+                modulo($matches[$index - 1], $matches[$index + 1], $result);
+            }
+            echo $result . "\n";
+        }
+    }
 }
